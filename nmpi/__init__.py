94e9d9b43ce41bd2333383f642d935b042726940
Andrew Davison <andrew.davison@unic.cnrs-gif.fr>
2017-03-20 16:11:15 +0100

    Release 0.5.0

--- nmpi/__init__.py
+++ nmpi/__init__.py
@@ -3,10 +3,14 @@ try:
     from .nmpi_saga import HardwareClient
 except ImportError:
     pass
+try:
+    from .nmpi_admin import AdminClient
+except ImportError:
+    pass
 
 SPINNAKER = "SpiNNaker"
 BRAINSCALES = "BrainScaleS"
 ESS = "BrainScaleS-ESS"
 SPIKEY = "Spikey"
 
-__version__ = "0.4.3"
+__version__ = "0.5.0"
